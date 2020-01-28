34143a8bb2647d83597d09697de08bb5372d33c5
Andrey, Orlov <Andrey.Orlov@excelian.com>
2020-01-28 20:55:14 +0300

    first part of education for Python dictionary

---
+++ DictionaryLesson.py
@@ -0,0 +1,42 @@
+# ---------------------------------------
+# Program by Orlov.A.
+#
+#
+# Version     Date      Info
+# 1.0         2016   Initial Version
+#
+# ----------------------------------------
+
+enemy = {
+    'loc_x': 70,
+    'loc_y': 50,
+    'color': 'green',
+    'health': 100,
+    'name': 'Mudillo',
+}
+
+print("Location X = " + str(enemy['loc_x']))
+print("Location Y = " + str(enemy['loc_y']))
+print("His name is: " + enemy['name'])
+
+enemy['rank'] = 'Admiral'
+print(enemy)
+
+del enemy['rank']
+print(enemy)
+
+enemy['loc_x'] = enemy['loc_x'] + 40
+enemy['health'] = enemy['health'] - 30
+if enemy['health'] < 80:
+    enemy['color'] = 'yellow'
+
+print(enemy)
+
+print(enemy.keys())
+print(enemy.values())
+
+enemyTwo = enemy
+
+enemy['color'] = 'red'
+print(enemy)
+print(enemyTwo)
